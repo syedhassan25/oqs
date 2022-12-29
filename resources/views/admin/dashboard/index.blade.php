6233d94837b8d03f2003ba8d65d887d57aaa6f85
syedhassan25 <syedhassan25@hotmail.com>
2022-12-29 16:46:41 +0500

    add change in dashboard

--- resources/views/admin/dashboard/index.blade.php
+++ resources/views/admin/dashboard/index.blade.php
@@ -51,7 +51,7 @@
     <!-- /.col -->
     <div class="col-12 col-sm-6 col-md-3">
       <div class="info-box mb-3">
-        <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-users"></i></span>
+        <span class="info-box-icon bg-warning elevation-1"><i class="fas fa-users"></i></span>
 
         <div class="info-box-content">
           <span class="info-box-text">Inactive Students</span>
